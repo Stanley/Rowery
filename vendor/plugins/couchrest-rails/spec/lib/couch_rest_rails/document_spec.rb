71a85efd36b57cabe8b93d473d0052d2464ab636
Stanisław Wasiutyński <staszek.wasiutynski@gmail.com>
2009-11-02 12:04:16 +0100

    couchrest-rails plugin installed

---
+++ vendor/plugins/couchrest-rails/spec/lib/couch_rest_rails/document_spec.rb
@@ -0,0 +1,29 @@
+require File.dirname(__FILE__) + '/../../spec_helper'
+
+describe CouchRestRails::Document do
+  
+  class CouchRestRailsTestDocument < CouchRestRails::Document 
+    use_database :foo
+  end
+  
+  before :each do
+    @doc = CouchRestRailsTestDocument.new
+  end
+  
+  it "should inherit from CouchRest::ExtendedDocument" do
+    CouchRestRails::Document.ancestors.include?(CouchRest::ExtendedDocument).should be_true
+  end
+  
+  it "should define its CouchDB connection and CouchDB database name" do
+    @doc.database.name.should == "#{COUCHDB_CONFIG[:db_prefix]}foo#{COUCHDB_CONFIG[:db_suffix]}"
+  end
+
+  describe '.unadorned_database_name' do
+    
+    it "should return the database name without the prefix and suffix" do
+      CouchRestRailsTestDocument.unadorned_database_name.should == 'foo'
+    end
+    
+  end
+  
+end
